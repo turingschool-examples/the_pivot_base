c55e23bb4420ae3df3e59c27d9ff911c57f6dc70
Hugh Morrison <hmorri32@gmail.com>
2017-12-12 13:42:23 -0700

    deletes duplicate test

--- spec/features/user/views/user_can_visit_category_show_spec.rb
+++ spec/features/user/views/user_can_visit_category_show_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
+
 RSpec.describe "As a visitor can visit category show page" do
-    it " can visit category show page" do
-        create_items
-        
-        visit '/cats'
-        
-    end
+  it " can visit category show page" do
+    create_items
+    visit '/cats'
+  end
 end
