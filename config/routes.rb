d25dd217a546c4187b2632f6e3ed3024cd253d61
Dione Wilson <dionew1@gmail.com>
2017-12-12 13:41:10 -0700

    Refactor Routes file and change carts routes to a cart route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,38 +3,25 @@ Rails.application.routes.draw do
   root :to => 'main#index'
 
   get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'auth/failure',            to: redirect('/')
+  get 'signout',                 to: 'sessions#destroy', as: 'signout'
 
-  get '/login', :to => 'sessions#new', :as => 'login'
-  post '/login', :to => 'sessions#create'
+  get '/login',     :to => 'sessions#new', :as => 'login'
+  post '/login',    :to => 'sessions#create'
   delete '/logout', :to => 'sessions#destroy'
 
 
   namespace :admin do
     resources :dashboard, only: [:index]
-    resources :items, only: [:index, :edit, :new, :create, :update]
+    resources :items,     only: [:index, :edit, :new, :create, :update]
     resources :analytics, only: [:index]
   end
 
-
-  resources :users , only: [:new, :create, :edit, :update]
-
-  resources :orders, only: [:index, :new, :show, :update]
-
+  resources :users ,    only: [:new, :create, :edit, :update]
+  resources :orders,    only: [:index, :new, :show, :update]
   resources :dashboard, only: [:index]
-
-  get '/cart', :to => 'carts#index', :as => 'cart'
-
-  resources :items, only: [:index, :show]
-
-  resources :carts, only: [:index, :create, :destroy]
-
-  patch '/cart', :to => 'carts#update'
-
-  delete '/cart', :to => 'carts#destroy'
-  resources :carts, only: [:index, :create, :destroy]
+  resources :items,     only: [:index, :show]
+  resource  :cart,      only: [:show, :create, :destroy]
 
   get '/:category', to: 'categories#show', param: :slug, as: "category"
-
 end
