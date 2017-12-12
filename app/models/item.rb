e9f3d0295b3649b9f0c9360cf7f5334cf80f6a91
Hugh Morrison <hmorri32@gmail.com>
2017-12-12 08:39:23 -0700

    includes paperclip shoulda matchers and rewrites item validations using shoulda matchers

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,22 @@
 class Item < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
 
   belongs_to :category
   has_many   :order_items
   has_many   :orders, through: :order_items
 
   has_attached_file :image,
-    :default_url => '/images/missing.jpg', styles: {thumb: "68x68#", medium: "300x300#"},
-    :path => ":rails_root/public/images/:id/:style/:filename",
-    :url => "/images/:id/:style/:filename"
-  validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/jpg', 'image/gif', 'image/png']
+                    :default_url => '/images/missing.jpg',
+                    :styles      =>  { thumb: "68x68#", medium: "300x300#" },
+                    :path        => ":rails_root/public/images/:id/:style/:filename",
+                    :url         => "/images/:id/:style/:filename"
+
+  validates_attachment_content_type :image,content_type: ['image/jpeg',
+                                                          'image/jpg',
+                                                          'image/gif',
+                                                          'image/png']
 
   enum condition: ["active", "retired"]
 
