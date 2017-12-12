e7166a8a590ffd6e7c12c6b92f5104519ad9b218
Hugh Morrison <hmorri32@gmail.com>
2017-12-12 08:33:48 -0700

    deletes factory bloat

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,18 @@
 class Item < ApplicationRecord
-  belongs_to :category
-  has_many :order_items
-  has_many :orders, through: :order_items
-  validates :title     ,  presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :description, :price, presence: true
+
+  belongs_to :category
+  has_many   :order_items
+  has_many   :orders, through: :order_items
+
   has_attached_file :image,
     :default_url => '/images/missing.jpg', styles: {thumb: "68x68#", medium: "300x300#"},
     :path => ":rails_root/public/images/:id/:style/:filename",
     :url => "/images/:id/:style/:filename"
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/jpg', 'image/gif', 'image/png']
-  enum condition: ["active", "retired"]
 
+  enum condition: ["active", "retired"]
 
   def self.total_sold_by_item
     group(:title).joins(:order_items).sum(:quantity)
