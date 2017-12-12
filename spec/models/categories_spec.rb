bd9cc9ed075c7205d6ab1e4e567e46879c8523c9
Hugh Morrison <hmorri32@gmail.com>
2017-12-12 08:48:31 -0700

    shoulda matcher refactor for category spec

--- spec/models/categories_spec.rb
+++ spec/models/categories_spec.rb
@@ -2,23 +2,10 @@ require 'rails_helper'
 
 describe Category do
   describe 'validations' do
-    describe 'invalid attributes' do
-      it "can't be created without a title" do
-        category = Category.new
-        expect(category).to be_invalid
-      end
-    end
-    describe 'valid attributes' do
-      it 'can be created with a title' do
-        category = Category.new(title: "Animals")
-        expect(category).to be_valid
-      end
-    end
+    it { should validate_presence_of(:title) }
   end
+
   describe "relationships" do
-    it "has many items" do
-      category = Category.new(title: "Animals")
-      expect(category).to respond_to(:items)
-    end
+    it { should have_many(:items) }
   end
 end
