a5b6334c6b4115b269a10f58d8fa0650803822f3
Liam Barstad <liambarstad@gmail.com>
2017-12-11 18:16:57 -0700

    add _item.html.erb partial in app/views/carts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     raw("#{link_to("Login", login_path, options)}<br/>\n#{link_to("Create new account", new_user_path, options)}")
   end
 
+  def user_name_possessive
+    if current_user
+      current_user.first_name + "'s"
+    else
+      "Your"
+    end
+  end
+
 end
