63333e3e9003c5e111945248644161ef0966721d
Hugh Morrison <hmorri32@gmail.com>
2017-12-12 08:39:23 -0700

    includes paperclip shoulda matchers and rewrites item validations using shoulda matchers

--- spec/models/items_spec.rb
+++ spec/models/items_spec.rb
@@ -4,33 +4,40 @@ describe Item do
   let(:category) { build(:category, title: "Animals") }
 
   describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:price) }
+    it { should have_attached_file(:image) }
+
+
     describe 'invalid attributes' do
-      it 'is invalid without a title' do
-        item = build(:item, title: nil)
-        expect(item).to be_invalid
-      end
+    #   it 'is invalid without a title' do
+    #     item = build(:item, title: nil)
+    #     expect(item).to be_invalid
+    #   end
 
-      it 'is invalid without a description' do
-        item = build(:item, description: nil)
-        expect(item).to be_invalid
-      end
+      # it 'is invalid without a description' do
+      #   item = build(:item, description: nil)
+      #   expect(item).to be_invalid
+      # end
 
-      it 'is invalid without a price' do
-        item = build(:item, price: nil)
-        expect(item).to be_invalid
-      end
+      # it 'is invalid without a price' do
+      #   item = build(:item, price: nil)
+      #   expect(item).to be_invalid
+      # end
 
-      it 'is valid without a image' do
-        item = build(:item, image: nil)
-        expect(item).to be_valid
-      end
+      # it 'is valid without a image' do
+      #   item = build(:item, image: nil)
+      #   expect(item).to be_valid
+      # end
 
-      it 'is invalid without a unique title' do
-        persisted_valid_item = create(:item, title: "Funsie Onesie")
-        invalid_item = build(:item, title: "Funsie Onesie")
-        expect(invalid_item).to be_invalid
-        expect(invalid_item.errors.full_messages).to include("Title has already been taken")
-      end
+      # it 'is invalid without a unique title' do
+      #   persisted_valid_item = create(:item, title: "Funsie Onesie")
+      #   invalid_item = build(:item, title: "Funsie Onesie")
+      #   expect(invalid_item).to be_invalid
+      #   expect(invalid_item.errors.full_messages).to include("Title has already been taken")
+      # end
     end
 
     describe 'valid attributes' do
