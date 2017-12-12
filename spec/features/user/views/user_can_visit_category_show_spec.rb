c5a31c260fdf1084fd3127ecba5da07c49ffdf4c
Hugh Morrison <hmorri32@gmail.com>
2017-12-12 14:03:40 -0700

    deletes slow_helper entirely

--- spec/features/user/views/user_can_visit_category_show_spec.rb
+++ spec/features/user/views/user_can_visit_category_show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor can visit category show page" do
   it " can visit category show page" do
-    create_items
+    Category.create(title: "Cats")
     visit '/cats'
   end
 end
