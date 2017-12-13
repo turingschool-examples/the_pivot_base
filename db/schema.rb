fa4a2eca5f7fea4aa7171cca872e4a3fc093823f
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-13 07:44:56 -0700

    Added nessecary twitter stuff to user model via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212210413) do
+ActiveRecord::Schema.define(version: 20171213144404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,10 @@ ActiveRecord::Schema.define(version: 20171212210413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.string "oauth_secret"
   end
 
   add_foreign_key "items", "categories"
