cddfbe4c9a15a69c48c2474e3be1b5c8f43acd97
Hugh Morrison <hmorri32@gmail.com>
2017-12-13 08:56:34 -0700

    spec compolete for platform admin approves store

--- spec/features/platform_admin/admin_approves_pending_store_spec.rb
+++ spec/features/platform_admin/admin_approves_pending_store_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "As a Platform admin " do
 
     before { login_user(admin.email, admin.password) }
 
-    it "I see a link called stores" do
+    it "I see a stores and I can approve a pending store" do
       expect(current_path).to eq('/admin/dashboard')
 
       click_on "Stores"
@@ -18,25 +18,16 @@ RSpec.feature "As a Platform admin " do
       expect(page).to have_content("Pending (1)")
       expect(page).to have_content('Suspended (1)')
       expect(page).to have_content('Active (1)')
-      
-      
 
+      click_on "Pending"
+
+      expect(current_path).to eq('/admin/stores/pending')
+
+      click_on pending.name
+      click_on "Approve Store"
+
+      expect(current_path).to eq('/admin/stores')
+      expect(page).to have_content('Active (2)')
     end
   end
 end
-
-# Background: 3 stores: (Vandelay Industries - pending), (Innotech - suspended), (Bluth Company - active)
-
-# As an admin
-# When I visit "/admin/dashboard"
-# And I click "Stores"
-# Then I should see a list of stores divided into sections by their status
-# And "pending" should be the first tab
-# And "active" should be the second tab
-# And "suspended" should be the last tab
-# And when I click "Approve" for the pending company
-# Then it shows up in the "active" tab
-
-# NOTE: There should be no option to delete stores. We want to maintain records of all stores even if they shut down.
-
-# enum status: %w(pending suspended active)
