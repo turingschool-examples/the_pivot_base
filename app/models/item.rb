81aeaeafb28835c1cdf77c43973f471cf59cdf73
Emily Wise <wise.m.emily@gmail.com>
2017-12-13 11:42:22 -0700

    updated many to many relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
+  belongs_to :store_items
+  has_many :stores, through: :store_items
   has_many :order_items
   has_many :orders, through: :order_items
   validates :title     ,  presence: true, uniqueness: true
