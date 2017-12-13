1e49134377e56fbface090f748621b451bfd9dfd
Hugh Morrison <hmorri32@gmail.com>
2017-12-12 20:34:35 -0700

    sequence stores fatory

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :store do
-    name "MyString"
-    status 1
-    slug "MyString"
+    sequence :name do |x|
+      "#{x}MyStore"
+    end
+
+    sequence :slug do |y|
+      "#{y}-my-store"
+    end
+    status 0
   end
 end
