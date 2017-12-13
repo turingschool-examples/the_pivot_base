722b97017e1e7136152447249e235c14700bee45
randy-darsh <rspringer47@gmail.com>
2017-12-12 17:57:06 -0700

    updating user controller to have flash message and test is now passing

--- spec/features/user/views/user_can_edit_their_information_without_having_to_change_their_password_spec.rb
+++ spec/features/user/views/user_can_edit_their_information_without_having_to_change_their_password_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe 'As a registered user, when i visit my account edit page' do
   before :each do
-    user = create(:user)
-    stub_logged_in_user(user)
+    @user = create(:user)
+    stub_logged_in_user(@user)
   end
+
   it 'i can edit my information withougt entering my password' do
     visit "/account/edit"
 
@@ -13,13 +14,10 @@ describe 'As a registered user, when i visit my account edit page' do
     click_on("Submit")
 
     expect(current_path).to eq("/account/edit")
-    expect(page).to have_content("George Oscar Bluth")
+
+    within("#user_first_name") do
+      expect(@user.first_name).to eq("George Oscar Bluth")
+    end
+    expect(page).to have_content("Successfully Updated Your Account Information")
   end
 end
-# As a logged in user
-# When I visit "/account/edit"
-# And I change my First Name to "George Oscar Bluth"
-# And I don't enter a password to edit my other data
-# And I click "Submit"
-# Then I should be on "/account/edit"
-# And I should see a my first name as "George Oscar Bluth"
