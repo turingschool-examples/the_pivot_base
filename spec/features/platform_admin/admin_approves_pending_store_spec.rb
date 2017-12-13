75696670f9822b3bf50dcc5aca83b9ba65daca89
Hugh Morrison <hmorri32@gmail.com>
2017-12-12 19:46:44 -0700

    test framework for admin sees pending stores etc

---
+++ spec/features/platform_admin/admin_approves_pending_store_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "As a Platform admin " do
+  describe "When I visit admin/dashboard" do
+    let!(:admin) { create(:admin) }
+    before       { login_user(admin.email, admin.password) }
+
+    it "I see a link called stores" do
+      expect(current_path).to eq('/admin/dashboard')
+      # click_on "Stores"
+
+    end
+  end
+end
+
+# Background: 3 stores: (Vandelay Industries - pending), (Innotech - suspended), (Bluth Company - active)
+
+# As an admin
+# When I visit "/admin/dashboard"
+# And I click "Stores"
+# Then I should see a list of stores divided into sections by their status
+# And "pending" should be the first tab
+# And "active" should be the second tab
+# And "suspended" should be the last tab
+# And when I click "Approve" for the pending company
+# Then it shows up in the "active" tab
+
+# NOTE: There should be no option to delete stores. We want to maintain records of all stores even if they shut down.
