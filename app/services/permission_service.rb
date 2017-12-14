3a82e06216d070a24713ca716fa80639edf9db52
Miguel Silva <msilva0897@gmail.com>
2017-12-13 17:59:43 -0700

    added permission services

--- app/services/permission_service.rb
+++ app/services/permission_service.rb
@@ -6,7 +6,17 @@ class PermissionService
   end
 
   def authorized?
-    #we will add
+    if user.registered_user?
+      registered_user_permissions
+    elsif user.store_manager?
+      store_manager_user_permissions
+    elsif user.store_admin?
+      store_admin_permissions
+    elsif user.platform_admin?
+      platform_admin_permissions
+    else
+      guest_user_permissions
+    end
   end
 
   private
@@ -14,6 +24,7 @@ class PermissionService
   attr_reader :user, :controller, :action
 
   def guest_user_permissions
+    return true if controller == "main" && action.in?(%w(index))
     return true if controller == "stores" && action.in?(%w(index show))
     return true if controller == "items" && action.in?(%w(index show)) #potentially nested resource with category or store
     return true if controller == "sessions" && action.in?(%w(new create destroy))
@@ -22,23 +33,40 @@ class PermissionService
   end
 
   def registered_user_permissions
-    return true if controller == "stores" && action.in?(%w(index show))
+    return true if controller == "main" && action.in?(%w(index))
+    return true if controller == "stores" && action.in?(%w(index show new create))
     return true if controller == "items" && action.in?(%w(index show)) #potentially nested resource with category or store
     return true if controller == "sessions" && action.in?(%w(new create destroy))
     return true if controller == "carts" && action.in?(%w(index create update destroy))
     return true if controller == "orders" && action.in?(%w(index create show )) #change controller method from new to create
     return true if controller == "users" && action.in?(%w(edit update show ))
-    return true if controller == "dashboard" && action.in?(%w(index ))
+    return true if controller == "dashboard" && action.in?(%w(index))
   end
 
   def store_manager_user_permissions
-    return true if controller == "stores" && action.in?(%w(index show))
+    return true if controller == "main" && action.in?(%w(index))
+    return true if controller == "stores" && action.in?(%w(index show new create))
     return true if controller == "items" && action.in?(%w(update edit index show)) #potentially nested resource with category or store
     return true if controller == "sessions" && action.in?(%w(new create destroy))
     return true if controller == "carts" && action.in?(%w(index create update destroy))
-    return true if controller == "orders" && action.in?(%w(index create show )) #change controller method from new to create
-    return true if controller == "users" && action.in?(%w(edit update show ))
-    return true if controller == "dashboard" && action.in?(%w(index ))
+    return true if controller == "orders" && action.in?(%w(index create show update)) #change controller method from new to create
+    return true if controller == "users" && action.in?(%w(edit update show))
+    return true if controller == "dashboard" && action.in?(%w(index))
   end
 
+  def store_admin_user_permissions
+    return true if controller == "main" && action.in?(%w(index))
+    return true if controller == "stores" && action.in?(%w(index show new create update edit))
+    return true if controller == "items" && action.in?(%w(update edit index show destroy new create)) #potentially nested resource with category or store
+    return true if controller == "sessions" && action.in?(%w(new create destroy))
+    return true if controller == "carts" && action.in?(%w(index create update destroy))
+    return true if controller == "orders" && action.in?(%w(index create show update)) #change controller method from new to create
+    return true if controller == "users" && action.in?(%w(edit update show))
+    return true if controller == "dashboard" && action.in?(%w(index))
+    return true if controller == "user_roles" && action.in?(%w(index new create update destroy))
+  end
+
+  def platform_admin
+    true
+  end
 end
