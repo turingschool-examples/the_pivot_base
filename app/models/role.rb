dbda962a68256354545195f83227668c581dc3db
Hugh Morrison <hmorri32@gmail.com>
2017-12-13 17:48:03 -0700

    fixes alignment

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,6 +3,5 @@ class Role < ApplicationRecord
 
   has_many :user_role_stores
   has_many :stores, through: :user_role_stores
-  has_many :users, through: :user_role_stores
-
+  has_many :users,  through: :user_role_stores
 end
