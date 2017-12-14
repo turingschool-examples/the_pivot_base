17c3e02051d0e54cf98178af17c1446fb10e1732
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-14 13:40:11 -0700

    Second authenticated admin spec passing

--- spec/features/admin/items/admin_can_create_items_spec.rb
+++ spec/features/admin/items/admin_can_create_items_spec.rb
@@ -24,20 +24,21 @@ RSpec.feature "Admin item creation" do
       expect(page).to have_content("59.99")
     end
 
-    xit "I can create an item without an image and it defaults" do
-      admin = build(:admin)
+    it "I can create an item without an image and it defaults" do
+      admin = create(:admin)
       category = create(:category)
+      store = create(:store, user: admin)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-      visit admin_items_path
+      visit store_items_path(store)
 
-      # click_on "Create New Item"
+      click_on "Create New Item"
       fill_in "item[title]", with: "Onesie"
       fill_in "item[description]", with: "This Onesie is awesome!"
       fill_in "item[price]", with: "59.99"
       click_on "Create Item"
 
-      expect(current_path).to eq(admin_items_path)
+      expect(current_path).to eq(store_items_path(store))
       expect(page).to have_content("Onesie")
       expect(page).to have_content("59.99")
     end
