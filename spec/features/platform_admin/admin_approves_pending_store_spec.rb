16d71b6638bd0c0311a767df6ed99587629a1ff9
Hugh Morrison <hmorri32@gmail.com>
2017-12-13 17:49:33 -0700

    refactor spec to accomodate for new user role paradigm

--- spec/features/platform_admin/admin_approves_pending_store_spec.rb
+++ spec/features/platform_admin/admin_approves_pending_store_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature "As a Platform admin " do
+  let!(:admin)          { create(:platform_admin) }
+  let!(:platform_admin) { Role.new(name: 'platform_admin') }
+  let!(:pending)        { create(:store, name: "Vandelay Industries") }
+  let!(:suspended)      { create(:store, name: "Innotech", status: 1) }
+  let!(:active)         { create(:store, name: "Bluth Company", status: 2) }
+  let!(:scrub_user)     { create(:user) }
+
   describe "When I visit admin/dashboard" do
-    let!(:admin)     { create(:platform_admin) }
-    let!(:pending)   { create(:store, name: "Vandelay Industries") }
-    let!(:suspended) { create(:store, name: "Innotech", status: 1) }
-    let!(:active)    { create(:store, name: "Bluth Company", status: 2) }
 
     before { login_user(admin.email, admin.password) }
 
     it "I see a stores and I can approve a pending store" do
       expect(current_path).to eq('/admin/dashboard')
+
+      admin.roles << platform_admin
 
       click_on "Stores"
 
@@ -28,6 +33,20 @@ RSpec.feature "As a Platform admin " do
 
       expect(current_path).to eq('/admin/stores')
       expect(page).to have_content('Active (2)')
+    end
+  end
+
+  describe "as a default user" do
+    before { login_user(scrub_user.email, scrub_user.password) }
+    it "i should receive status code 404 and a not found page" do
+       expect { visit '/admin/dashboard'}.to raise_error(ActionController::RoutingError)
+    end
+  end
+
+  describe "as a default admin user" do
+    before { login_user(admin.email, admin.password) }
+    it "i should receive status code 404 and a not found page" do
+       expect { visit '/admin/dashboard/stores'}.to raise_error(ActionController::RoutingError)
     end
   end
 end
