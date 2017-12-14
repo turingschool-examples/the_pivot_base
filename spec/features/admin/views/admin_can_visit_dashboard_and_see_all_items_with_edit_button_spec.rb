378cfddc5381e92c5df7bc2c5ddf97b59347576e
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-14 14:28:42 -0700

    Another one

--- spec/features/admin/views/admin_can_visit_dashboard_and_see_all_items_with_edit_button_spec.rb
+++ spec/features/admin/views/admin_can_visit_dashboard_and_see_all_items_with_edit_button_spec.rb
@@ -6,18 +6,16 @@ RSpec.describe 'an admin can visit admin dashboard' do
       admin_user = User.create(first_name: "Admin", last_name: "McAdmin", email: "admin@admin.com", password: "boom", role: "admin")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
       category = create(:category, title: 'Cats')
-      @item_one = create(:item, category_id: category.id)
-      @item_two = create(:item, category_id: category.id)
+      store = create(:store, user: admin_user)
+      @item_one = create(:item, category_id: category.id, store: store)
+      @item_two = create(:item, category_id: category.id, store: store)
 
-      visit admin_dashboard_index_path
-
-      click_on "View Items"
+      visit store_items_path(store)
 
       expect(page).to have_content(@item_one.title)
       expect(page).to have_content(@item_one.price)
       expect(page).to have_content(@item_two.title)
       expect(page).to have_content(@item_two.price)
-      expect(page).to have_content("Edit")
     end
   end
 end
