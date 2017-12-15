0ade81f2b94208011a71a3511ffc756cc4b8cdc2
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-15 11:32:19 -0700

    Added show view for stores, and slugged store name

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,11 +2,19 @@ class Store < ApplicationRecord
   has_many   :items
   belongs_to :user
 
-  before_save :add_url
-
+  # before_save :add_url
+  before_save :generate_slug
   enum status: [ :pending, :active, :suspended ]
 
-  def add_url
-    self.url = name.parameterize
+  def to_param
+    slug
   end
+
+  def generate_slug
+    self.slug ||= name.parameterize
+  end
+  # def add_url
+  #   self.url = name.parameterize
+  # end
+
 end
