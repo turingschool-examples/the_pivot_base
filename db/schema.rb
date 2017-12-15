8baedbed2db0c8c352ef8cb9d2d3fd2f405965cf
Aurora Ziobrowski <aziobrow@gmail.com>
2017-12-15 16:14:20 -0700

    changed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214005513) do
+ActiveRecord::Schema.define(version: 20171215230613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,23 +67,23 @@ ActiveRecord::Schema.define(version: 20171214005513) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "store_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.bigint "store_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_store_users_on_role_id"
+    t.index ["store_id"], name: "index_store_users_on_store_id"
+    t.index ["user_id"], name: "index_store_users_on_user_id"
+  end
+
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "url"
-  end
-
-  create_table "user_roles", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "role_id"
-    t.bigint "store_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["role_id"], name: "index_user_roles_on_role_id"
-    t.index ["store_id"], name: "index_user_roles_on_store_id"
-    t.index ["user_id"], name: "index_user_roles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 20171214005513) do
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
-  add_foreign_key "user_roles", "roles"
-  add_foreign_key "user_roles", "stores"
-  add_foreign_key "user_roles", "users"
+  add_foreign_key "store_users", "roles"
+  add_foreign_key "store_users", "stores"
+  add_foreign_key "store_users", "users"
 end
