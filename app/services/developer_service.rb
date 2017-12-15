4b0f72937a5270b371f75ae982b4df4cc5eb1cd5
Liam Barstad <liambarstad@gmail.com>
2017-12-15 11:59:00 -0700

    developer_service/make_developer_role no longer needs arguement

--- app/services/developer_service.rb
+++ app/services/developer_service.rb
@@ -5,21 +5,22 @@ class DeveloperService
   end
 
   def register_as_developer
-    if @user && !@user.developer?
+    if user && !user.developer?
       key = generate_key
-      @user.create_api_key(key: key)
-      raise ArguementError if !@user.api_key
-      make_developer_role(@user)
+      user.create_api_key(key: key)
+      raise ArguementError if !user.api_key
+      make_developer_role
     end
   end
 
   def generate_key
-    Digest::SHA512.hexdigest(Time.now.to_s + @user.email)
+    Digest::SHA512.hexdigest(Time.now.to_s + user.email)
   end
 
   private
+    attr_reader :user
 
-    def make_developer_role(user)
+    def make_developer_role
       user.roles << Role.find_or_create_by(name: 'developer')
     end
 
