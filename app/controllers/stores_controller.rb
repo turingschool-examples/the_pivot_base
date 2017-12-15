08f2e965263cb47c493446aa2649b0d7e64940c7
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-15 08:48:09 -0700

    Fix approve/suspend button for admin stores

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,16 +1,23 @@
 class StoresController < ApplicationController
 
-	def index
-    @stores	=	current_user.stores
-	end 
+  def index
+    @stores = current_user.stores
+  end 
 
-	def new
-		@store = Store.new
-	end 
+  def new
+    @store = Store.new
+  end 
 
-	def create
-		@category = Category.new(title: params["store"]["name"] )
-		@category.save
-		redirect_to dashboard_index_path
-	end 
+  def create
+    @category = Category.new(title: params["store"]["name"] )
+    @category.save
+    redirect_to dashboard_index_path
+  end
+
+  def update
+    @store = Store.find(params[:id])
+    @store.update!(status: params[:status])
+    @store.save!
+    redirect_back(fallback_location: root_path)
+  end
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213152531) do
+ActiveRecord::Schema.define(version: 20171215014938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
