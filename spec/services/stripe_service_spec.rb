c30ec4390cac958c6274981aeda86f72b879c0a7
Liam Barstad <liambarstad@gmail.com>
2017-12-15 11:54:07 -0700

    wip

---
+++ spec/services/stripe_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe StripeService do
+  context "making a payment" do
+    it "#process payment" do
+
+    end
+
+    it "with no info" do
+
+    end
+
+    it "with invalid info" do
+
+    end
+
+    it "without money on card" do
+
+    end
+  end
+end
