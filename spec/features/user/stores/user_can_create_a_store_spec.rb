1e5e131df0469bab222855aec7a63881a9bf186a
Aaron Schreck <schreck.aaron@gmail.com>
2017-12-14 20:33:31 -0700

    indentation fix

--- spec/features/user/stores/user_can_create_a_store_spec.rb
+++ spec/features/user/stores/user_can_create_a_store_spec.rb
@@ -2,21 +2,21 @@ require "rails_helper"
 
 feature "User can create a new store" do
   it "once they have logged in" do
-  user = create(:user, first_name: "Josh")
+    user = create(:user, first_name: "Josh")
 
-  stub_logged_in_user(user)
+    stub_logged_in_user(user)
 
-  visit '/stores/new'
+    visit '/stores/new'
 
-  fill_in "store[name]", with: "Vandelay Industries"
+    fill_in "store[name]", with: "Vandelay Industries"
 
-  click_on "Create Store"
+    click_on "Create Store"
 
-  expect(current_path).to eq '/dashboard'
-  expect(page).to have_content "My Stores"
+    expect(current_path).to eq '/dashboard'
+    expect(page).to have_content "My Stores"
 
-  click_on "My Stores"
+    click_on "My Stores"
 
-  expect(page).to have_content("Vandelay Industries")
+    expect(page).to have_content("Vandelay Industries")
   end 
 end 
