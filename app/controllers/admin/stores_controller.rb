bb6f9476e87ef9b4fb595d8b1463a911a2b34798
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-14 20:46:39 -0700

    wip

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -1,7 +1,6 @@
 class Admin::StoresController < ApplicationController
   before_action :require_admin
   def index
-    binding.pry
     @stores = []
     @stores << Store.where(status: 'pending') if params[:query] == 'pending' || params[:query].nil?
     @stores << Store.where(status: 'active') if params[:query] == 'active'
