4377163e517a55603cd8d1d0be00ff30c08a101b
Miguel Silva <msilva0897@gmail.com>
2017-12-15 20:11:07 -0700

    added the platform admin controllers and updated the sessions to accept the platform admin dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -31,7 +31,9 @@ class SessionsController < ApplicationController
   def login_successful
     session[:user_id] = @user.id
     flash[:notice] = "Logged in as #{@user.first_name} #{@user.last_name}"
-    if @user.store_admin?
+    if @user.platform_admin?
+      redirect_to platform_admin_dashboard_path
+    elsif @user.store_admin?
       redirect_to admin_dashboard_index_path
     elsif @user.registered_user?
       redirect_to dashboard_index_path
