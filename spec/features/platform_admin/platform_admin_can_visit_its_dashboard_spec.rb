395b2d0f198e943e703c0077bd3e9ffdc8ea9242
Miguel Silva <msilva0897@gmail.com>
2017-12-15 20:10:13 -0700

    added a test for platform admin

--- spec/features/platform_admin/platform_admin_can_visit_its_dashboard_spec.rb
+++ spec/features/platform_admin/platform_admin_can_visit_its_dashboard_spec.rb
@@ -5,15 +5,18 @@ describe 'when a platform admin visits its dashboard' do
     @platform_admin = create(:platform_admin, email: "platform_admin@example.com")
     role = create(:role, name: "store_admin")
     store = create(:store)
-    create(:user_role, user: @platform_admin, role: role, store: store)
+    create(:store_user, user: @platform_admin, role: role, store: store)
+    3.times do
+      create(:store)
+    end
   end
   it "sees all the stores in the page" do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@platform_admin)
-    visit platform_admin_dashboard
+    login_user(@platform_admin.email, @platform_admin.password)
+    visit platform_admin_dashboard_path
 
     expect(current_path).to eq("/platform_admin/dashboard")
     expect(page).to have_content("Manage Accounts")
     expect(page).to have_content("Gob")
-    expect(page).to have_content("MyString")
+    expect(page).to have_content("MyString1")
   end
 end
