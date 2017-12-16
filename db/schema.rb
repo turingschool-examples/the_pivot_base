016eeb7bc403038151f45c59d3d71d2dbd2ed900
Aaron Schreck <schreck.aaron@gmail.com>
2017-12-16 11:17:22 -0700

    Remove user roles from user and import previous status via rake

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216164601) do
+ActiveRecord::Schema.define(version: 20171216175211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,7 +105,6 @@ ActiveRecord::Schema.define(version: 20171216164601) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 0
     t.string "uid"
     t.string "name"
     t.string "oauth_token"
