4046ad74d09b00f27befea521d944acc06e375f6
Hugh Morrison <hmorri32@gmail.com>
2017-12-16 15:19:23 -0700

    adds authorize method to json requests

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,8 +1,20 @@
-class Api::V1::BaseController < ActionController::API
+class Api::V1::BaseController < ApplicationController
   include Response
   include ExceptionHandler
 
+  before_action :authenticate!
+
   def route_not_found
-    render :json => {:error => "not found"}, :status => 404
+    render json: { error: "not found" }, status: 404
+  end
+
+  def authenticate!
+    json_response({ error: 'Invalid Credentials' }, 401) unless authorized?
+  end
+
+  def authorized?
+    if current_user && current_user.developer?
+      params[:api_key] && params[:api_key] == current_user.api_key[:key]
+    end
   end
 end
