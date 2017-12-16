c46d16084f4350ea6118b06eeaebb8a94bc652df
Aaron Schreck <schreck.aaron@gmail.com>
2017-12-15 20:17:10 -0700

    should fix failing store test once the store status is figured out.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171215175846) do
+ActiveRecord::Schema.define(version: 20171215220535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 20171215175846) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "url"
-    t.integer "status", default: 0
+    t.string "status", default: "Pending"
     t.bigint "user_id"
     t.string "slug"
     t.index ["slug"], name: "index_stores_on_slug"
