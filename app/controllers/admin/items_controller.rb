8bf513639b7cb2200de1f0c5a6498387f76b6ee5
Dione Wilson <dionew1@gmail.com>
2017-12-17 15:49:17 -0700

    Complete path replacement, now checking tests.

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,20 @@
 class Admin::ItemsController < ApplicationController
   before_action :require_admin
+  before_action :find_store
 
   def index
-    store = Store.find_by(slug: params["store_slug"])
     @store_items = StoreItemPresenter.new(store)
   end
 
   def new
-    @item = Item.new
+    @item = Item.new(store: store)
   end
 
   def create
     @categories = Category.all
     @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_items_path
+      redirect_to admin_store_items_path(store)
     else
       render :new
     end
@@ -25,7 +25,7 @@ class Admin::ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
-      redirect_to admin_items_path
+      redirect_to admin_store_items_path(store)
     else
       render :edit
     end
@@ -36,12 +36,17 @@ class Admin::ItemsController < ApplicationController
   end
 
   private
+  attr_reader :store
 
-  def item_params
-    params.require(:item).permit(:title, :description, :price, :image, :category_id, :store_id)
-  end
+    def find_store
+      @store = Store.find_by(slug: params["store_slug"])
+    end
 
-  def require_admin
-    render file: "/public/404" unless current_admin?
-  end
+    def item_params
+      params.require(:item).permit(:title, :description, :price, :image, :category_id, :store_id)
+    end
+
+    def require_admin
+      render file: "/public/404" unless current_admin?
+    end
 end
