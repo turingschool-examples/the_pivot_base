2670382d0429973b15f2a98d60f5c734886f30f7
Liam Barstad <liambarstad@gmail.com>
2017-12-17 14:11:55 -0700

    remove sad path (it's all happy)

--- spec/services/stripe_service_spec.rb
+++ spec/services/stripe_service_spec.rb
@@ -13,16 +13,5 @@ RSpec.describe StripeService do
       expect(charge).to be_instance_of(Charge)
     end
 
-    it "with no info" do
-
-    end
-
-    it "with invalid info" do
-
-    end
-
-    it "without money on card" do
-
-    end
   end
 end
