d62aecb97200d56454e70884bc23d5649009281b
Hugh Morrison <hmorri32@gmail.com>
2017-12-17 13:59:48 -0700

    fixes JSON format in accordance with spec

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -18,22 +18,3 @@ class Api::V1::SearchController < Api::V1::BaseController
       json_response({ message: "Missing params. This endpoint is hit like /api/v1/search?type=ITEMS&q=DESCRIPTION"}, :not_found)
     end
 end
-
-# {
-#   "type": "items",
-#   "q": "diapers",
-#   "results": [
-#     {
-#       "id": 1,
-#       "title": "Nature's Diapers",
-#       "description": "No chemicals or dyes. Also, they don't really work.",
-#       "price": "$30.00"
-#     },
-#     {
-#       "id": 103,
-#       "title": "Dopo Tesigns Shoulder Bag",
-#       "description": "Versitile bag great for commuting. Even makes as an excellent diaper bag. #dadcore #momcore",
-#       "price": "$125.00"
-#     }
-#   ]
-# }
