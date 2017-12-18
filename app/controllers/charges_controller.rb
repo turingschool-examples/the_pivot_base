c29586e9ac8c7307a22b1a16ec99faa69be452e3
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-18 12:50:02 -0700

    Added seamless checkout

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -10,7 +10,14 @@ class ChargesController < ApplicationController
     charge = StripeTool.create_charge(customer_id: customer.id,
                                       amount: @amount,
                                       description: @description)
-    redirect_to thanks_path
+    order = Order.create(status: "paid", user_id: current_user.id)
+    item_hash = @cart.cart_items
+    item_hash.each do |item, quantity|
+      order.order_items.create(quantity: quantity, unit_price: item.price, item: item)
+    end
+    @cart.destroy
+    flash[:success] = "Order was successfully placed"
+    redirect_to orders_path
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
@@ -21,11 +28,11 @@ class ChargesController < ApplicationController
 
   private
 
-    def amount_to_be_charged
-      @amount = params[:amount]
-    end
+  def amount_to_be_charged
+    @amount = params[:amount]
+  end
 
-    def description
-      @description = "Placeholder description"
-    end
+  def description
+    @description = "Placeholder description"
+  end
 end
