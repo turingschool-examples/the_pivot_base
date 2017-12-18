e571556dcb65833dd04291f36144d51ed3af4f07
Aurora Ziobrowski <aziobrow@gmail.com>
2017-12-17 18:13:15 -0700

    building form to edit store

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -3,4 +3,9 @@ class Admin::StoresController < ApplicationController
   def show
     @store = Store.find_by(url: params[:store_name])
   end
+
+  def edit
+    @store = Store.find_by(url: params[:store_name])
+    @statuses = Store.statuses.keys
+  end
 end
---
+++ spec/features/admin/stores/admin_can_edit_store_details_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature "an admin can update store info" do
+  let(:admin) { create(:store_admin) }
+  let(:store) { admin.stores.first }
+
+  before do
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user)
+      .and_return(admin)
+    visit admin_store_path(store.url)
+    click_on('Update Store Information')
+  end
+
+  it "they see a form to edit the store" do
+    expect(current_path).to eq(admin_edit_store_path(store.url))
+  end
+end
