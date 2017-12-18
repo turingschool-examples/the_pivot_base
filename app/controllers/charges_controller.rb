40c80dcf7ff76bd13323c3c058b827edb8876610
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-18 10:29:34 -0700

    Thanks page exists now

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,21 +5,28 @@ class ChargesController < ApplicationController
   end
 
   def create
-    customer = StripeTool.create_customer(email: params[:stripeEmail], 
+    customer = StripeTool.create_customer(email: params[:stripeEmail],
                                           stripe_token: params[:stripeToken])
 
-    charge = StripeTool.create_charge(customer_id: customer.id, 
+    charge = StripeTool.create_charge(customer_id: customer.id,
                                       amount: @amount,
-                                      description: 'Rails Stripe customer')
-
+                                      description: @description)
+    redirect_to thanks_path
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
 
+  def thanks
+  end
+
   private
 
-    def amount_to_be_charged
-      @amount = 500
-    end
+  def amount_to_be_charged
+    @amount = 500
+  end
+
+  def description
+    @description = "Some amazing product"
+  end
 end
