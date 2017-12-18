be2a700e0f921c950309563ef3178c2b7f9bc3e3
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-18 10:20:09 -0700

    Generated charges controller and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+  resources :charges, only: [:new, :create]
+
   namespace :admin do
     resources :dashboard, only: [:index]
     resources :items, only: [:index, :edit, :new, :create, :update]
