20d9c6d17a00a4d8ff20cd6976ebcea167cf609e
Emily Wise <wise.m.emily@gmail.com>
2017-12-17 17:13:10 -0700

    added platform can create an item

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -39,6 +39,22 @@ FactoryBot.define do
       end
     end
 
+    factory :platform_admin_with_store_items, class: User do
+      after(:create) do |user|
+        role = create(:role, name: "platform_admin")
+        store = create(:store_with_items)
+        create(:store_user, user: user, role: role, store: store)
+      end
+    end
+
+    factory :platform_admin_with_store_item, class: User do
+      after(:create) do |user|
+        role = create(:role, name: "platform_admin")
+        store = create(:store_with_item)
+        create(:store_user, user: user, role: role, store: store)
+      end
+    end
+
     factory :platform_admin, class: User do
       after(:create) do |user|
         role = create(:role, name: "platform_admin")
---
+++ spec/features/admin/stores/items/admin_can_create_items_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+feature " Store Admin can create an item" do
+  before(:each) do
+    @admin = create(:store_admin_with_store_item)
+  end
+
+  context "As an authenticated store admin" do
+    it "I can create an item" do
+      login_user(@admin.email, @admin.password)
+
+      visit admin_store_path(@admin.stores.first.url)
+      click_on "See all Items"
+
+      expect(current_path).to eq(admin_store_items_path(@admin.stores.first.url))
+
+      click_on "Create New Item"
+
+      fill_in "item[title]", with: "Onesie"
+      fill_in "item[description]", with: "This Onesie is awesome!"
+      fill_in "item[price]", with: "59.99"
+      page.attach_file("item[image]", testing_image)
+      click_on "Create Item"
+
+      expect(current_path).to eq(admin_store_items_path(@admin.stores.first.url))
+
+      expect(page).to have_content("Onesie")
+      expect(page).to have_content("59.99")
+    end
+
+    it "I can create an item without an image and it defaults" do
+      login_user(@admin.email, @admin.password)
+
+      visit admin_store_path(@admin.stores.first.url)
+      click_on "See all Items"
+
+      click_on "Create New Item"
+
+      fill_in "item[title]", with: "Onesie"
+      fill_in "item[description]", with: "This Onesie is awesome!"
+      fill_in "item[price]", with: "59.99"
+      click_on "Create Item"
+
+      expect(current_path).to eq(admin_store_items_path(@admin.stores.first.url))
+      expect(page).to have_content("Onesie")
+      expect(page).to have_content("59.99")
+    end
+  end
+end
+feature "Platform Admin can create an item" do
+  before(:each) do
+    @platform_admin = create(:platform_admin_with_store_item)
+  end
+
+  context "As an authenticated platform_admin admin" do
+    it "I can create an item" do
+      login_user(@platform_admin.email, @platform_admin.password)
+
+      visit admin_store_path(@platform_admin.stores.first.url)
+      click_on "See all Items"
+
+      expect(current_path).to eq(admin_store_items_path(@platform_admin.stores.first.url))
+
+      click_on "Create New Item"
+
+      fill_in "item[title]", with: "Onesie"
+      fill_in "item[description]", with: "This Onesie is awesome!"
+      fill_in "item[price]", with: "59.99"
+      page.attach_file("item[image]", testing_image)
+      click_on "Create Item"
+
+      expect(current_path).to eq(admin_store_items_path(@platform_admin.stores.first.url))
+
+      expect(page).to have_content("Onesie")
+      expect(page).to have_content("59.99")
+    end
+
+    it "I can create an item without an image and it defaults" do
+      login_user(@platform_admin.email, @platform_admin.password)
+
+      visit admin_store_path(@platform_admin.stores.first.url)
+      click_on "See all Items"
+
+      click_on "Create New Item"
+
+      fill_in "item[title]", with: "Onesie"
+      fill_in "item[description]", with: "This Onesie is awesome!"
+      fill_in "item[price]", with: "59.99"
+      click_on "Create Item"
+
+      expect(current_path).to eq(admin_store_items_path(@platform_admin.stores.first.url))
+      expect(page).to have_content("Onesie")
+      expect(page).to have_content("59.99")
+    end
+  end
+end
