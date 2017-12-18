860467ef0f98b9d852f927a5e7130543c43d07f2
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-18 10:25:26 -0700

    Setting up for dynamic amount

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,25 @@
 class ChargesController < ApplicationController
+  before_action :amount_to_be_charged
+
   def new
   end
 
   def create
-    @amount = 500
+    customer = StripeTool.create_customer(email: params[:stripeEmail], 
+                                          stripe_token: params[:stripeToken])
 
-    customer = Stripe::Customer.create(
-      email: params[:stripeEmail],
-      source: params[:stripeToken]
-    )
-
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: @amount,
-      description: 'Rails Stripe customer',
-      currency: 'usd'
-    )
+    charge = StripeTool.create_charge(customer_id: customer.id, 
+                                      amount: @amount,
+                                      description: 'Rails Stripe customer')
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
+  private
+
+    def amount_to_be_charged
+      @amount = 500
+    end
 end
