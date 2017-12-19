b38d013f0631f731719961bb822e5496498f4cf0
randy-darsh <rspringer47@gmail.com>
2017-12-19 13:35:10 -0700

    removed friendly id from chatroom

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,6 +3,4 @@ class Chatroom < ApplicationRecord
   has_many :users, through: :messages
   validates :topic, presence: true, uniqueness: true, case_sensitive: false
 
-  extend FriendlyId
-  friendly_id :topic, use: :slugged
 end
