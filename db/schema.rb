cbe504adbe67d3d7fd976376d76cc05f2806bfd8
Hugh Morrison <hmorri32@gmail.com>
2017-12-18 17:23:06 -0700

    adds twitter roles on user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171217201732) do
+ActiveRecord::Schema.define(version: 20171218235150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,10 @@ ActiveRecord::Schema.define(version: 20171217201732) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "screen_name"
+    t.string "uid"
+    t.string "oauth_token"
+    t.string "oauth_token_secret"
   end
 
   add_foreign_key "api_keys", "users"
