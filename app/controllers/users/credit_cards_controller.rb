55221b5a8d692796737677ed703519d3525ce19f
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-18 18:41:00 -0700

    Added correct link to nav and added form

--- app/controllers/users/credit_cards_controller.rb
+++ app/controllers/users/credit_cards_controller.rb
@@ -2,4 +2,21 @@ class Users::CreditCardsController < ApplicationController
   def new
     CreditCard.new
   end
+
+  def create
+    binding.pry
+    @user = current_user
+    @cc = CreditCard.new(credit_card_params)
+    if @cc.save
+      redirect_to credit_card_path(@cc)
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def credit_card_params
+      params.require(:credit_card).permit(:last4digits, :expiration_month, :expiration_year)
+    end
 end
--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,2 +1,8 @@
 class CreditCard < ApplicationRecord
+  def save_credit_card_details(last4digits, expiration_month, expiration_year)
+    self.last4digits = last4digits
+    self.expiration_month = expiration_month
+    self.expiration_year = expiration_year
+    self.save!
+  end
 end
