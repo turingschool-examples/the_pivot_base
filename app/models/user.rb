9a2b1f68fa83e38c3a02a59e7a57ec731d75ce95
Hugh Morrison <hmorri32@gmail.com>
2017-12-18 17:22:12 -0700

    adds self.from omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,20 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :password, presence: true
   validates :email, presence: true, uniqueness: true
 
+  def self.from_omniauth(auth_info)
+    where(uid: auth_info[:uid]).first_or_create do |user|
+      user.uid                = auth_info.uid
+      user.first_name         = auth_info.extra.raw_info.name.split[0]
+      user.last_name          = auth_info.extra.raw_info.name.split[1]
+      user.screen_name        = auth_info.extra.raw_info.screen_name
+      user.oauth_token        = auth_info.credentials.token
+      user.oauth_token_secret = auth_info.credentials.secret
+      user.email              = auth_info.extra.raw_info.screen_name
+      user.password           = 'i_heart_burritos'
+      user.save!
+    end
+  end
+
   def platform_admin?
     roles.exists?(name: 'platform admin')
   end
@@ -44,5 +58,4 @@ class User < ApplicationRecord
   def self.user_quantity_of_items_ordered
     group(:email).joins(orders: :order_items).sum(:quantity)
   end
-
 end
