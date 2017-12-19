c13c4f3e1c87bd4b553ef331b0f0ab55a7d78c56
randy-darsh <rspringer47@gmail.com>
2017-12-19 14:02:22 -0700

    put friendly id back

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,4 +3,6 @@ class Chatroom < ApplicationRecord
   has_many :users, through: :messages
   validates :topic, presence: true, uniqueness: true, case_sensitive: false
 
+  extend FriendlyId
+  friendly_id :topic, use: :slugged
 end
