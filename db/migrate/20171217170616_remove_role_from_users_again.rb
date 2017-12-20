626ded1adbebf1649024113ea242e1cf29ee9c53
Hugh Morrison <hmorri32@gmail.com>
2017-12-20 09:51:44 -0700

    remove remove role from user migration

--- db/migrate/20171217170616_remove_role_from_users_again.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRoleFromUsersAgain < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :role, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20171218235150) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
     t.string "screen_name"
     t.string "uid"
     t.string "oauth_token"
