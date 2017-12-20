79991ee3d2a35c4901c1fa7770861649303d7809
Erika Smith <erikaannesmith@gmail.com>
2017-12-19 18:54:12 -0700

    Cloudinary is functional, still l need to make rake task to fix existing items

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,10 @@ class Item < ApplicationRecord
   validates :title     ,  presence: true, uniqueness: true
   validates :description, :price, presence: true
   has_attached_file :image,
-    :default_url => '/images/missing.jpg', styles: {thumb: "68x68#", medium: "300x300#"},
-    :path => ":rails_root/public/images/:id/:style/:filename",
-    :url => "/images/:id/:style/:filename"
+    styles: { thumb: "68x68#", medium: "300x300#" },
+    default_url: '/images/missing.jpg',
+    path: ':id/:style/:filename',
+    storage: :cloudinary
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/jpg', 'image/gif', 'image/png']
   enum condition: ["active", "retired"]
 
