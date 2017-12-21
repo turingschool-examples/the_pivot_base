eb81beaf173d2423265f59cc37ec5836684c5f81
Liam Barstad <liambarstad@gmail.com>
2017-12-20 21:33:46 -0700

    fix migrations

--- db/migrate/20171219013512_add_credit_card_number_to_charge.rb
+++
@@ -1,5 +0,0 @@
-class AddCreditCardNumberToCharge < ActiveRecord::Migration[5.1]
-  def change
-    add_column :charges, :credit_card_number, :string
-  end
-end
--- db/migrate/20171221022315_change_user_uid_to_customer_id.rb
+++
@@ -1,6 +0,0 @@
-class ChangeUserUidToCustomerId < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :uid
-    add_column :users, :customer_id, :string
-  end
-end
