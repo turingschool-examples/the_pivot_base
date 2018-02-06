7cd22b1d8fb0c98ee25c5277828d2258db5ad654
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-06 16:02:06 -0700

    deleted commented out setup from removing the slowhelper

--- spec/features/user/authentication/user_cannot_view_another_users_data_spec.rb
+++ spec/features/user/authentication/user_cannot_view_another_users_data_spec.rb
@@ -2,20 +2,6 @@ require 'rails_helper'
 
 RSpec.feature "Authenticated users security" do
   context "As a logged in user" do
-    before(:each) do
-      # setup
-
-      # visit root_path
-
-      # click_on "Login"
-
-      # fill_in "session[email]", with: "jake@adventuretime.com"
-      # fill_in "session[password]", with: "dog"
-
-      # within(".action") do
-      # click_on("Login")
-      # end
-    end
     scenario "I cannot view another user's order" do
       chino = create(:user, first_name: "Chino")
       khaki = create(:user, first_name: "Khaki")
