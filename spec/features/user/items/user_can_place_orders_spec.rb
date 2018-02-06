aaa8fdeb276083bca24edc592fcb051dbd7e4369
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-06 16:03:38 -0700

    removed commented out bit from removing slowhelper

--- spec/features/user/items/user_can_place_orders_spec.rb
+++ spec/features/user/items/user_can_place_orders_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "User can place an order" do
   it "and see the message 'order was successfully placed'" do
-
-    # user = User.create(first_name: "Tester", last_name: "McTesty", email: "testerson@testmail.com", password: "testing", address: "dummy address")
+)
     user = create(:user, email: "testerson@testmail.com", password: "testing")
 
     item_one = create(:item)
