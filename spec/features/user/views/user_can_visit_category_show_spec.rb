489919d61b7af6e89c016965e231083983de8ece
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-06 15:50:41 -0700

    updated category test to use factories for items and category, remove slow helper methods

--- spec/features/user/views/user_can_visit_category_show_spec.rb
+++ spec/features/user/views/user_can_visit_category_show_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 RSpec.describe "As a visitor can visit category show page" do
     it " can visit category show page" do
-        create_items
-        
+      category = create(:category, title: "cats")
+        item_one = create(:item, category: category)
+        item_two = create(:item, category: category)
+
         visit '/cats'
-        
+
     end
 end
