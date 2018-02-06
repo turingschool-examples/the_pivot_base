93ec9eae1e9d1e01033cd859911473223d4cabda
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-06 15:49:30 -0700

    fixed 3 tests to use factories and remove slow helper dependencies

--- spec/features/user/authentication/unauthentic_user_cannot_view_another_users_data_spec.rb
+++ spec/features/user/authentication/unauthentic_user_cannot_view_another_users_data_spec.rb
@@ -1,22 +1,23 @@
 require 'rails_helper'
 
 RSpec.feature "Unauthenticated users security" do
-  before(:each) do
-    setup
-  end
+
   context "As an unauthenticated user" do
     it "I cannot view another user’s private data" do
+      order = create(:order)
       visit dashboard_index_path
 
       expect(current_path).to eq(login_path)
 
-      visit order_path(@order)
+      visit order_path(order)
 
       expect(current_path).to eq(login_path)
     end
 
     it "I should be redirected to login/create account when I try to check out" do
-      visit item_path(@unicorn_onesie_1)
+      unicorn_onesie_1 = create(:item)
+
+      visit item_path(unicorn_onesie_1)
 
       click_on "Add to cart"
 
