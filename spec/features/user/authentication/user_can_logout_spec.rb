8017182caa568611fb8b0953a467ce10cc74a845
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-06 16:01:06 -0700

    updated test to use user factory

--- spec/features/user/authentication/user_can_logout_spec.rb
+++ spec/features/user/authentication/user_can_logout_spec.rb
@@ -2,11 +2,11 @@
 
 RSpec.describe "As a visitor I can login" do
 	it "as a user I can log out" do
-		user = User.create(first_name: "Tester", last_name: "McTesty", email: "testerson@testmail.com", password: "testing")
+    user = create(:user)
 		visit '/'
 		click_on 'Login'
-		fill_in "session[email]", with: "testerson@testmail.com"
-		fill_in "session[password]", with: "testing"
+		fill_in "session[email]", with: user.email
+		fill_in "session[password]", with: user.password
 
 		within(".action") do
 			click_on("Login")
