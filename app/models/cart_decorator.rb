15f878b0ee78c34f8b22e99734d34be522cfaee0
BLaurenB <blbillington1@gmail.com>
2018-02-06 23:56:44 -0700

    trying more things

--- app/models/cart_decorator.rb
+++ app/models/cart_decorator.rb
@@ -2,7 +2,7 @@ class CartDecorator < SimpleDelegator
 
   #have access here to all methods in Cart
 
-  def items_with_quantity
+  def items_with_quantity #BUGGY bc a hash can't have 2 identical keys...might have to change back to Item as the key
     contents.inject({}) do |result, (item_id, quantity)|
       result[quantity] = Item.find(item_id)
       result
@@ -20,3 +20,37 @@ class CartDecorator < SimpleDelegator
   end #?
 
 end
+
+
+
+=begin
+
+  or instead of inheriting from delegator, I do something like this:
+
+class CartDecorator
+
+  def initialize(cart) --->(pass the Object...or try inheritance?)
+    @cart_contents_hash = cart.contents (so that I have all the methods?)
+    @cart_contents_hash.each do |id, qty|
+      CartItem.new(Item.find(id), qty) ---> make a CartItem class that has a qty method. does it make a new one each time we load a page, and makes a DB call?
+    end
+  end
+
+  def other_methods?
+
+  end
+
+
+end (of the class)
+
+
+BUT THIS MAKES AN EXTRA CLASS AND SO MUCH MORE THAN THE HASH METHOD ABOVE.
+
+
+
+
+
+
+
+
+=end
