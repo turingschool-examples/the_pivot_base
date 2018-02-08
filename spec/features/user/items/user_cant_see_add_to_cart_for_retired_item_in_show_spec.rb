8b27775fee4c79d774c91b5e186721861242eee1
nico24687 <theonlynico@hotmail.com>
2018-02-07 21:30:11 -0700

    down to three failign tests, blockers to review

--- spec/features/user/items/user_cant_see_add_to_cart_for_retired_item_in_show_spec.rb
+++ spec/features/user/items/user_cant_see_add_to_cart_for_retired_item_in_show_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe 'As a visitor' do
 			category = create(:category)
 			one_url = "http://pandathings.com/wp-content/uploads/2016/10/onesie-6-300x300.png"
 			item_one = category.items.create(title: "Funsie Onesie", description: "number one", price: 8.00,
-			image: one_url, condition: 'retired' )
+			image: one_url, condition: 'retired', store: create(:store) )
 
-			visit item_path(item_one)
+			visit store_item_path(item_one.store, item_one)
 
 			expect(page).not_to have_content("Add to cart")
 		end
