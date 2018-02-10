46dcedc1ee99e4398eb7a2070e6cae6ce9e56771
BLaurenB <blbillington1@gmail.com>
2018-02-10 12:42:52 -0700

    fixing links

--- app/services/permission.rb
+++ app/services/permission.rb
@@ -41,7 +41,7 @@ class Permission
 
     def store_admin_permissions
       return true if controller == "sessions" && action.in?(%w(new create destroy))
-      return true if controller == 'carts' && action.in?(%w(index create update destory))
+      return true if controller == 'carts' && action.in?(%w(index create update destroy))
       return true if controller == 'stores' && action.in?(%w(index))
       return true if controller == 'stores/items' && action.in?(%w(index show))
       return true if controller == 'categories' && action.in?(%w(index))
@@ -69,7 +69,7 @@ class Permission
       return true if controller == "sessions" && action.in?(%w(new create destroy))
       return true if controller == 'stores/items' && action.in?(%w(index))
       return true if controller == 'categories' && action.in?(%w(index))
-      return true if controller == 'carts' && action.in?(%w(index create update destory))
+      return true if controller == 'carts' && action.in?(%w(index create update destroy))
       return true if controller == 'orders' && action.in?(%w(index show))
       return true if controller == 'dashboard' && action.in?(%w(index))
 
@@ -79,7 +79,7 @@ class Permission
       return true if controller == 'stores' && action.in?(%w(index))
       return true if controller == "sessions" && action.in?(%w(new create destroy))
       return true if controller == 'stores/items' && action.in?(%w(index show))
-      return true if controller == 'categories' && action.in?(%w(index))
+      return true if controller == 'categories' && action.in?(%w(index show))
       return true if controller == 'carts' && action.in?(%w(index show new create edit update destroy))
     end
 end
