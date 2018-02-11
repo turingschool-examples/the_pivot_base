e60e65b95c9707136ff2ed95a473fff876b93ba6
nico24687 <theonlynico@hotmail.com>
2018-02-10 20:19:47 -0700

    now redirected to the root path if user tries to visit store with status of inactive

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208233418) do
+ActiveRecord::Schema.define(version: 20180211030641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20180208233418) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 1
   end
 
   create_table "user_roles", force: :cascade do |t|
