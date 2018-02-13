9e081bc836fc6eae1653bb17d26581a5dd9bf6cf
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-12 17:55:17 -0700

    working on platform admin tests

--- spec/features/platform_admin/god_can_log_in_and_out_spec.rb
+++ spec/features/platform_admin/god_can_log_in_and_out_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
-describe "Platform Admin logs in and out" do
+RSpec.describe "As a Platform Admin I can login and logout" do
+  it "I login and see my dashboard" do
+    god = create(:platform_admin)
+    role = create(:role, title: "platform_admin")
+    user_role = create(:user_role, user: god, role: role)
 
-  xit "they log in" do
 
+    visit '/'
+    click_on 'Login'
+    fill_in "session[email]", with: god.email
+    fill_in "session[password]", with: god.password
+
+    within(".action") do
+      click_on("Login")
+    end
+
+    expect(current_path).to eq(admin_dashboard_index_path)
+    expect(page).to have_content("You're logged in as an Administrator")
+
+    click_on 'Logout'
+    expect(page).to have_content("Login")
   end
-
-  xit "they log out" do
-
-  end
-
-
 end
