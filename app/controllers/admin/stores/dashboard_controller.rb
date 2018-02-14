ed45cf7a91b4203a0cad36c350543661faf13305
nico24687 <theonlynico@hotmail.com>
2018-02-13 18:51:50 -0700

    Now when you log in as corey you are able to update the status of a store and can update orders etc

--- app/controllers/admin/stores/dashboard_controller.rb
+++ app/controllers/admin/stores/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::Stores::DashboardController < ApplicationController
 
   def index
-    @store = current_user.store
+    @store = Store.find_by(slug: params[:store])
     if params[:status]
       #fix me
       @orders = Order.filter_by_status(params[:status])
