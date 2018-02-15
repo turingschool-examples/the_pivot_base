1902dfeccc62b78f09b8c754a33fbd3bcf2fbf59
BLaurenB <blbillington1@gmail.com>
2018-02-15 14:40:24 -0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :address)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :address, :address_2, :city, :state, :zip, :phone)
   end
 
 end
